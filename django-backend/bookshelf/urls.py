96c9f75a02c29eddefaea4bb9aed1db9e9b9b81d
marco19-sketch <marco.brusca1970@gmail.com>
2026-06-28 18:51:07 +0200

    Chore: added a home.html as a welcome page for the admin site

--- django-backend/bookshelf/urls.py
+++ django-backend/bookshelf/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 # from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
     path('api/v1/', include('catalog.urls')),
     path('api/v1/', include('log_reg.urls')),
